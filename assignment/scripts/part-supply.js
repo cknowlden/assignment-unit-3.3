10baeabe71fdb9d681d95e33371e56e86f3b4c04
Cali Knowlden <calbearnc@gmail.com>
2023-11-13 19:36:47 -0600

    answered questions 3&4

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -23,7 +23,8 @@ console.log('The second item is', secondItem);
 // 4. The last value in the 'supplyChanges' array was added by mistake.
 //    Remove it from the array and store it inside a new variable called 'removedItem'.
 console.log('4. Remove the last value from supplyChanges:');
-
+let removedItem = supplyChanges.pop();
+console.log('The removed item is', removedItem);
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Add the value 25 into supplyChanges.');
